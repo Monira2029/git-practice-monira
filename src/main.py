62a624989c564f55805ea2d52fbc26e9f68c84ac
Monira2029 <msultana25799@gmail.com>
2026-06-13 17:19:17 +0600

    Added multiplication function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 print('Name : Monira Sultana')
 
 print("Today's Date : ", date.today())
 
-print("Addition:", add(10, 5))
-print("Subtraction:", subtract(10, 5))
+print("Addition:", add(2, 3))
+print("Subtraction:", subtract(15, 5))
+print("Multiplication:", multiply(3, 5))
